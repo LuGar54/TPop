08171c9a82480e261afcbca2105b08d3f79e0aff
Ludovic Garon <ludovicgaron54@gmail.com>
2025-01-21 17:12:16 -0500

    added code for malus law

---
+++ 1 Polarisation/graphLoiMalus.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+angles =    [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180]
+intensity = []
+
+plt.plot(intensity, angles)
+
+plt.show()
