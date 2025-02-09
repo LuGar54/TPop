b8a60cb783d4debe17ffa9d9d0d7e4b5b4fa6498
Adam Pelletier <adpel11@ulaval.ca>
2025-02-09 11:35:28 -0500

    Finished code for graphs

--- 3 Fibre/graphIntensityOverlay.py
+++ 3 Fibre/graphIntensityOverlay.py
@@ -1,14 +1,24 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas
+from PIL import Image
+import matplotlib.patheffects as pe
 
 
-img = plt.imread("patternCrop.jpg")
+#img = plt.imread("Monomode.jpg", )
 
-csvFile = pandas.read_csv('Multimode_haut.csv', usecols=['Gray_Value'])
+im =np.array(Image.open('Hautement monomode.png').convert('L'))
+csvFileHM = pandas.read_csv('Multimode_haut.csv', usecols=['Gray_Value'])
+csvFileLM = pandas.read_csv('Leger multimode.csv', usecols=['Gray_Value'])
+csvFileM = pandas.read_csv('monomode.csv', usecols=['Gray_Value'])
 
-plt.imshow(img, origin='lower', extent=[0, len(csvFile), 0, 100], aspect='auto')
+
+plt.style.use('grayscale')
+plt.imshow(im, origin='lower' ,extent=[0, len(csvFileHM), 0, 100], aspect='auto')
 
 
-plt.plot(csvFile)
-
+plt.plot(csvFileHM,path_effects=[pe.Stroke(linewidth=5, foreground='w'), pe.Normal()])
+plt.title("Hautement monomode")
+plt.xlabel('Position des pixels')
+plt.ylabel('Intensité [%]')
 plt.show()
