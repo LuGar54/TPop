9e0ff32997a6ef2f7f0ee7532d709df6b79beb7b
Adam Pelletier <adpel11@ulaval.ca>
2025-02-18 11:44:05 -0500

    Skibidi

--- 4 Diffraction/graphDiffraction.py
+++ 4 Diffraction/graphDiffraction.py
@@ -2,11 +2,13 @@ import pandas
 import matplotlib.pyplot as plt
 
 
-csvDots = pandas.read_csv('SimpleFenteInstagramReels.csv', usecols=['Gray_Value'])
+csv1F = pandas.read_csv('TEST.csv', usecols=['Gray_Value'])
 csv2F4D = pandas.read_csv('DoubleFenteInstagramReels.csv', usecols=['Gray_Value'])
+
+
 #X =range(len(csv2F4D))
 #print(X)
 
-plt.plot(csvDots)
-plt.plot(csv2F4D)
+plt.plot(csv1F)
+plt.plot(csv2F4D-50)
 plt.show()
