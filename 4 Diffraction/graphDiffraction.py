7944610760bbea6e44d9e026c1206647f8042d7c
Adam Pelletier <adpel11@ulaval.ca>
2025-02-18 11:20:42 -0500

    New values for graph

--- 4 Diffraction/graphDiffraction.py
+++ 4 Diffraction/graphDiffraction.py
@@ -2,7 +2,11 @@ import pandas
 import matplotlib.pyplot as plt
 
 
-csvDots = pandas.read_csv('DiffractionDots.csv', usecols=['Gray_Value'])
+csvDots = pandas.read_csv('SimpleFenteInstagramReels.csv', usecols=['Gray_Value'])
+csv2F4D = pandas.read_csv('DoubleFenteInstagramReels.csv', usecols=['Gray_Value'])
+#X =range(len(csv2F4D))
+#print(X)
 
 plt.plot(csvDots)
+plt.plot(csv2F4D)
 plt.show()
